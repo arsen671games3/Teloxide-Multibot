64ae9ea805c4279031cef0e89bb7c9db530a194a
Arseny <ars>
2024-01-02 23:56:56 +0300

    Fixing

--- src/main.rs
+++ src/main.rs
@@ -21,25 +21,30 @@ fn make_bot(token: &String) -> Bot{
 
 #[tokio::main]
 async fn main() {
-    let bot_tokens: Vec<String> = serde_json::from_str(BOT_TOKENS).expect("BOT_TOKENS error");
-    let addr = ([0, 0, 0, 0], PORT).into();
-    let url = format!("http://127.0.0.1:{PORT}/webhook/").parse().unwrap();
+    let listeners_and_bots;
+    {
+        let bot_tokens: Vec<String> = serde_json::from_str(BOT_TOKENS).expect("BOT_TOKENS error");
+        let addr = ([0, 0, 0, 0], PORT).into();
+        let url = format!("http://127.0.0.1:{PORT}/webhook/").parse().unwrap();
+    
+        println!("Starting... {:?}", bot_tokens);
+    
+        let bots: Vec<Bot> = bot_tokens.iter().map(make_bot).collect();
+        let usernames: Vec<String> = join_all(bots.iter().map(|bot| async {
+            let username = bot.get_me().await.expect("Bot is not working").username.clone().unwrap();
+            format!("@{username}")
+        })).await;
+        println!("Started {:?}", usernames);
+        let mut options = webhooks::Options::new(addr, url);
+        let (stop_token, senders) = axum_app::make_axum_app(&mut options);
+        listeners_and_bots = join_all(bot_tokens.iter().zip(bots).map(|(bot_token, bot)| async {
+            (axum_app::start_bot(bot.clone(), bot_token, &stop_token, &senders, &options)
+            .await
+            .expect("Error while starting bot"), bot)
+        })).await;
+    }
 
-    println!("Starting... {:?}", bot_tokens);
-
-    let bots: Vec<Bot> = bot_tokens.iter().map(make_bot).collect();
-    let usernames: Vec<String> = join_all(bots.iter().map(|bot| async {
-        let username = bot.get_me().await.expect("Bot is not working").username.clone().unwrap();
-        format!("@{username}")
-    })).await;
-    println!("Started {:?}", usernames);
-    let mut options = webhooks::Options::new(addr, url);
-    let (stop_token, senders) = axum_app::make_axum_app(&mut options);
-
-    join_all(bot_tokens.iter().zip(bots).map(|(bot_token, bot)| async {
-        let listener = axum_app::start_bot(bot.clone(), bot_token, &stop_token, &senders, &options)
-        .await
-        .expect("Error while starting bot");
+    join_all(listeners_and_bots.into_iter().map(|(listener, bot)| async {
         teloxide::repl_with_listener(
             bot,
             |bot: Bot, msg: Message| async move {
